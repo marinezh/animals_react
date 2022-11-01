4f6db57023bcb53db98dfe30448132b197921c7f
Marina Zhivotova <mazhivotova@gmail.com>
2022-11-01 13:45:41 +0200

    add animals app

---
+++ src/Main.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Main.css";
+
+class Main extends Component {
+    render() {
+        return (
+            <main>
+                <input type="search" />
+                <div className="Container">
+
+                </div>
+        </main>
+    )
+}
+}
+
+export default Main;
