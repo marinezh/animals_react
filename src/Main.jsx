688e33a7ed584a4206aaafe5ea22b059f0796791
Marina Zhivotova <mazhivotova@gmail.com>
2022-11-02 14:49:40 +0200

    add js part done

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,28 +1,93 @@
 import React, { Component } from "react";
 import { animals } from "./animals";
-import Card from "./UI/Card"
+import Card from "./UI/Card";
 import "./Main.css";
 
 class Main extends Component {
-    state = {
-        animals: animals,
-    };
+  state = {
+      animals: animals,
+      search: "",
+  };
 
-    animalList = animals.map(animal => <Card name={animal.name} likes={ animal.likes} />)
+  removeHandler = (theClickedName) => {
+    console.log("remove was clicked", theClickedName);
+    const updatedArray = this.state.animals.filter(
+      (animal) => animal.name !== theClickedName
+    );
+
+    this.setState({ animals: updatedArray });
+  };
+
+  addHandle = (name) => {
+    console.log("add was clicked");
+    this.setState((state) => {
+      const updatedArray = state.animals.map((animal) => {
+        if (animal.name === name) {
+          return { ...animal, likes: animal.likes + 1 };
+        } else {
+          return animal;
+        }
+      });
+      return {
+        animals: updatedArray,
+      };
+    });
+  };
+
+  removeHandle = (name) => {
+    console.log("remove was clicked", name);
+    this.setState((state) => {
+      const updatedArray = state.animals.map((animal) => {
+        if (animal.name === name) {
+          return { ...animal, dislikes: !animal.dislikes ? -1 :animal.dislikes -1 };
+        } else {
+          return animal;
+        }
+      });
+      return {
+        animals: updatedArray,
+      };
+    });console.log(this.state.animals);
+  };
+    
+    
+  searchHandler = (e) => {
+    this.setState({ search: e.target.value });
+  };
+
     render() {
-        const Cards = () => {
-          
-      }
+    //     const newAnimalList = this.state.animals.map((animal) => {
+    //         return ({...animal, dislikes: 0})
+    //   })
+
+        
+    const animalFilter = this.state.animals.filter((animal) => {
+      console.log("ffff");
+      return animal.name.includes(this.state.search);
+    });
+
+    // this.state.animals below replaced with animalFilter
+    const animalList = animalFilter.map((animal) => {
+      return (
+        <Card
+          key={animal.name}
+          name={animal.name}
+          likes={animal.likes}
+          dislikes={animal.dislikes}
+          removeCard={() => this.removeHandler(animal.name)}
+              addLikes={() => this.addHandle(animal.name)}
+              removeLikes={()=> this.removeHandle(animal.name)}
+        />
+      );
+    });
+
     return (
       <main>
-            <h1>Animals</h1>
-            {this.animalList}
-        <input type="search" />
-        <div className="CardsContainer">
-                {/* <Card>
-                    name={animals.name}
-          </Card> */}
-        </div>
+        <h1>{this.state.animals.length} animals</h1>
+        <h1>{this.state.search}</h1>
+
+        <input onChange={this.searchHandler} type="search" />
+        <div className="Cards_container">{animalList}</div>
       </main>
     );
   }
--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import myStyle from "./Card.css";
+import  "./Card.css";
 
 const Card = (props) => {
-  console.log(props.name);
+  // console.log(props.name);
   return (
-    <div className="Card">
+    <div className="card"
+    >
       <img
-        src={`https://source.unsplash.com/500x400/?${props.name}`}
+        src={`https://source.unsplash.com/400x500/?${props.name}`}
         alt={props.name}
       />
-      <button>remove</button>
-      <h2>{props.name}</h2>
-      <div className="AddLike">
-        
-              <p>💚 {props.likes}</p> 
-              <button>Add like</button>
-       
+      <button className="delete_card" onClick={props.removeCard}>x</button>
+      <h3>{props.name}</h3>
+      <div className="add_like">
+        <button className="like_btn" onClick={props.addLikes}>💜 {props.likes}</button>
+        <button className="disLike_btn" onClick={props.removeLikes}>💔  {!props.dislikes ? 0 : props.dislikes}</button>
+        {/* <button onClick={props.addLikes}>Add like</button> */}
       </div>
     </div>
   );
